f2cbb351f6f1506f7eaa4c8858a7bbc03949c47d
adarsh251 <mishraadarsh25104@gmail.com>
2024-11-20 15:29:31 +0530

    export app in server.js

--- server.js
+++ server.js
@@ -179,6 +179,11 @@ console.log("blogs route");
 app.use('/api', blogRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
