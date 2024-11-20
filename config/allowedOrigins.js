4d46c4b539af8b97c3a5264d3460b2b2c7b070a1
adarsh251 <mishraadarsh25104@gmail.com>
2024-11-21 00:11:14 +0530

    cors website re-update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,12 @@
 const allowedOrigins=[
     'http://localhost:5173',
     'https://mme-website-frontend.vercel.app/',
+    'https://mme-website-frontend.vercel.app',
     'https://mme-website-fronten-git-c1e41b-adarsh-mishras-projects-c53eeb4c.vercel.app/',
-    'https://mme-website-frontend-lvm3jxq4q-adarsh-mishras-projects-c53eeb4c.vercel.app/'
+    'https://mme-website-fronten-git-c1e41b-adarsh-mishras-projects-c53eeb4c.vercel.app',
+    'https://mme-website-frontend-lvm3jxq4q-adarsh-mishras-projects-c53eeb4c.vercel.app/',
+    'https://mme-website-frontend-adarsh-mishras-projects-c53eeb4c.vercel.app/',
+    'https://mme-website-frontend-lvm3jxq4q-adarsh-mishras-projects-c53eeb4c.vercel.app',
 ];
 
 module.exports=allowedOrigins;
